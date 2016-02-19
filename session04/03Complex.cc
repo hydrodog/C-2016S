d7ba74c255cb27dc8e66ef1ae607c2e9347f9844
Dov Kruger <Dov.Kruger@gmail.com>
2016-02-19 17:25:50 -0500

    added homework complex problem, make it compile and run

--- session04/03Complex.cc
+++ session04/03Complex.cc
@@ -1,8 +1,10 @@
 int main() {
-	Complex c1; // (real=0, imag =0)
-	Complex c2(1.5);// real=1.5, imag = 0.0
+	const Complex c1; // (real=0, imag =0)
+	const Complex c2(1.5);// real=1.5, imag = 0.0
 	Complex c3(1.5,2.25); // real = 1.5, imag=2.25
 
   c1.print(); // 0+0i
 	c3.print(); // 1.5+2.25i
-	
+	Complex c4 = c1.add(c2); // (real=1.5, imag=0.0)
+	c4.print();
+}
